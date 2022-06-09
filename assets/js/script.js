465c893959f1509990991f3ec9f601a8e5557e11
hamomgs <hamomgomer947@gmail.com>
2022-06-09 18:25:21 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let name = 'Hamom';
+let age = 19;
+let year = 2022 - age;
+console.log(age + year);
+console.log('Estou mexendo no console.log');
+
+let quartaFeira = false;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
