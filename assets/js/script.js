ed59e8a0e0d41c127740032e494e0654ada70ff4
hamomgs <hamomgomer947@gmail.com>
2022-06-13 22:29:59 -0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,26 @@
-// EXERCÍCIO COM CONSOLE
+// 1 - 1 a 10
 
-// let age = 19;
+for (let i = 1; i <= 10; i++) {
+	console.log(i)
+}
 
-// if (age > 18) {
-//     console.log('Você é maior de idade.');
+// 2 - 10 a 1
+
+// let i = 10;
+
+// while (i >= 1) {
+// 	console.log(i);
+// 	i--;
 // }
 
-// let human = true;
+// 3 - impares 1 a 100
 
-// if (age > 18 && human == true) {
-//     console.log('Você pode acessar o site. ^^')
+// for (let i = 1; i <= 100; i += 2) {
+// 	console.log(i);
 // }
 
-// let birthdayMonth = 1;
+// 4 - pares 0 a 100 
 
-// if (birthdayMonth == 1 || birthdayMonth == 12) {
-//     console.log('Você faz aniversário no mês premiado!');
-// } 
-
-
-// EXERCÍCIO BÔNUS NO CONSOLE
-
-// let name = 'Rafael';
-// let lastName = 'Ribeiro';
-
-// if (name[0] == 'R') {
-// 	console.log('Seu nome começa com R.');
-// } 
-
-// if (lastName.length > 6 || name[0] == 'E') {
-// 	console.log('Seu nome começa com E ou seu sobrenome tem 6 letras.');
+// for (let i = 0; i <= 100; i += 2) {
+// 	console.log(i);
 // }
-
-
-// EXERCÍCIO COM PROMPT/ALERT/CONFIRM
-
-let name = prompt('Qual seu nome?');
-let lastName = prompt('Qual seu sobrenome?');
-let age = prompt('Qual sua idade?');
-
-if (age > 18) {
-    alert(`Uau você já tem ${age} anos, já não é mais uma criança.`);
-} else {
-	alert(`Nossa, você tem ${age} anos, cuidado com os sites que acessa.`)
-}
-
-let human = confirm(`${name} se você for mesmo humano, selecione OK...`);
-
-if (age > 18 && human == true) {
-    alert('Humm então você é humano e maior de idade...');
-} else if (human == false) {
-	alert('Sempre acreditei em Aliens...')
-} else if (human == true) {
-	alert('Que bom que você é humano, nunca dúvidei.')
-}
-
-alert('Estamos premiando quem faz aniversário nos meses premiados, vejamos se você é um sortudo...')
-
-let birthdayMonth = prompt('Qual mês do seu aniversário? \nResponda com o número do mês ex.: 4');
-
-if (birthdayMonth == 1 || birthdayMonth == 12) {
-    alert('Você faz aniversário no mês premiado!');
-} else {
-    alert('Poxa, seu aniversário não é no mês premiado...');
-}
-
-if (name[0] == 'R') {
-	alert('Seu nome começa com R, então você ganha uma bala.');
-} 
-
-if (lastName.length > 6 || name[0] == 'E') {
-	alert('Seu sobre nome é grande ou seu nome começa com E? Você ganha um emoji: 🦆');
-}
-
-alert(`${name}, foi um prazer ter você aqui, independente se você ganhou ou perdeu.`);
